61c2666db2fc208d8e4b4188189fc6360b207c4a
App Academy Student <student@appacademy.io>
2019-09-10 15:18:26 -0700

    Inheritance tests failed

--- W4/D2/Class_Inheritance/employee.rb
+++ W4/D2/Class_Inheritance/employee.rb
@@ -1,3 +1,13 @@
 class Employee
+  attr_accessor :name, :title, :salary, :boss # not all should be accesible
+  def initialize(name, title, salary, boss)
+    @name, @title, @salary, @boss = name, title, salary, boss
+  end
+
+  def bonus(multiplier) 
+    @salary * multiplier
+  end
+
+  # boss instance method
   
 end
--- W4/D2/Class_Inheritance/manager.rb
+++ W4/D2/Class_Inheritance/manager.rb
@@ -1,2 +1,28 @@
-class Manager
+require_relative("employee.rb")
+
+class Manager < Employee
+  attr_accessor :employees # Quick Fix
+  def initialize(employees, name, title, salary, boss)
+    @employees = employees
+    # super # please work
+    super(name, title, salary, boss)
+  end
+
+  def bonus(multiplier) 
+    total_salary = employees.map(&:salary).sum
+    total_salary * multiplier
+  end
+
 end
+
+
+ned = Manager.new([], "Ned", "Founder", 10**6, nil)
+darren = Manager.new([], "Darren", "Manager", 78_000, ned) 
+david = Employee.new("David", "TA", 10 ^ 4, darren)
+shawna = Employee.new("Shawna", "TA", 12 * 10 ^ 3, darren)
+ned.employees = [david, shawna]
+darren.employees = [darren]
+
+p ned.bonus(5) #== 500_000
+p darren.bonus(4) == 88_000
+p david.bonus(3) == 30_000
