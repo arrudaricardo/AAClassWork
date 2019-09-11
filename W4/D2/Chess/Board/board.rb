01100d6c0464d1e024152cd0adf2a840bc0fe586
App Academy Student <student@appacademy.io>
2019-09-11 09:30:24 -0700

    Folders Renamed

--- W4/D2/Chess/Board/board.rb
+++ W4/D2/Chess/Board/board.rb
@@ -5,11 +5,10 @@ class Board
 
   attr_reader :grid
 
-  def initialize()
+  def initialize
     @rows = Array.new(8) { Array.new(8) } 
     # @sentinel = NullPiece # later
   end 
-  
 
   def [](arr)
     x, y = arr
@@ -27,31 +26,32 @@ class Board
     raise "Not a valid move" unless valid_pos?(empty_pos) # check
   end
 
-
-  
   def valid_pos?(piece)
  #piece on board
  end
   
   def add_piece(piece, pos)
+
   end  
   
   def checkmate?(color)
+
   end  
 
   def in_check?(color)
+
   end  
   
   def find_king(color)
+
   end  
 
-  def pieces
-  end 
-
   def dup
+
   end
 
   def move_piece!(color, start_pos, end_pos)
+
   end
 
 end
