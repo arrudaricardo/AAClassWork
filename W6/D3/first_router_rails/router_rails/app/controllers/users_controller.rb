cf61003078f1b763687224d39cc9bfbe1947a771
App Academy Student <student@appacademy.io>
2019-09-25 17:55:40 -0700

    Added resources in controller

--- W6/D3/first_router_rails/router_rails/app/controllers/users_controller.rb
+++ W6/D3/first_router_rails/router_rails/app/controllers/users_controller.rb
@@ -19,6 +19,15 @@ class UsersController < ApplicationController
     render json: user
   end
 
+  def update
+    user = User.find(params[:id])
+    if user.update(user_param)
+      render json: user
+    else
+      render json: user.errors.full_messages, status: 422
+    end
+  end
+
   def destroy
     user = User.find(params[:id])
     user.destroy
